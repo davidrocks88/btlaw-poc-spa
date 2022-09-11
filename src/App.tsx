5a2b980d8742ad4ddf96ea575d93c9b112c230bc
davidrocks88 <D1a0v9i4dX1e0n9o4>
2022-09-11 17:06:47 -0500

    Add search bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Organization, useOrganizations } from './Organization';
 import { Tag } from './Tag';
-import { Link } from 'react-router-dom';
 
 export function Temp() {
   return <p>Temp</p>
 }
 
-interface IFiltersState {
-  [tag: string]: boolean
-}
-
 export function Home() {
   const { data: organizations } = useOrganizations()
   const [filters, setFilters] = useState<any>({})
+  const [searchString, setSearchString] = useState<string>('')
 
   function updateFilter(name: string, shouldFilter: Boolean) {
-    console.log({ name, shouldFilter })
     setFilters({
       ...filters,
       [name]: shouldFilter
@@ -45,14 +40,25 @@ export function Home() {
 
   let tags = organizations?.map(o => o.tags).flat().sort((t1, t2) => t1.trim().toLocaleLowerCase().localeCompare(t2.trim().toLocaleLowerCase())).filter((v, i, a) => a.indexOf(v) === i)
 
+  if (searchString.length > 0) {
+    orgsToShow = orgsToShow?.filter((org) => org.name.toLowerCase().includes(searchString.toLowerCase()) || org.description.toLowerCase().includes(searchString.toLowerCase()) || org.tags.join('').toLowerCase().includes(searchString.toLowerCase()))
+  }
+
+
   return (
     <div>
-      <div className='mt-2 flex flex-row flex-wrap gap-y-2 items-end align-middle content-center'>
+      <div className='mt-1 flex flex-row flex-wrap gap-y-2 items-end align-middle content-center'>
         <div className='cursor-pointer border-2 hover:bg-gray-100 border-white rounded-full p-2 font-lg font-bold' onClick={() => setFilters({})}>Reset</div>
-
         {tags?.map(t => <div className='mb-3'><Tag filters={filters} name={t} handleClick={updateFilter} /></div>)}
       </div>
       <hr />
+
+      <div className="relative w-1/4 m-2">
+        <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+          <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+        </div>
+        <input onChange={(event) => setSearchString(event.target.value)} type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Organization, Descriptions, Tags..." required />
+      </div>
 
       <div className='flex flex-row flex-wrap'>
         {orgsToShow?.map(org => <Organization organization={org} />)}
--- src/index.tsx
+++ src/index.tsx
@@ -26,10 +26,10 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <div className=''>
-          <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
+          <nav className="flex items-center justify-between flex-wrap bg-[#131829] p-6">
             <div className="flex items-center flex-shrink-0 text-white mr-6">
-              <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z" /></svg>
-              <span className="font-semibold text-xl tracking-tight">BT Pro Bono Proof Of Concept</span>
+              <img className='h-8' src='https://btlaw.com/-/media/images/btlaw/site-logo/btlawlogo.ashx?h=69&w=411&la=en&hash=C3C4E5F6805DC4719AF72237AD0B82E1' />
+              <span className="ml-2 font-semibold text-xl tracking-tight">Pro Bono HQ</span>
             </div>
             <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
               <div className="text-sm lg:flex-grow">

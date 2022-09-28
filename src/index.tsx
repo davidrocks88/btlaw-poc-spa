2ff7ca7740816c7a9f978d75dcbcda68361884c0
davidrocks88 <D1a0v9i4dX1e0n9o4>
2022-09-27 22:46:33 -0500

    more clean

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,7 @@ import { App } from './App';
 import reportWebVitals from './reportWebVitals';
 import {
   BrowserRouter,
+  Link,
   Routes,
   Route
 } from "react-router-dom";
@@ -19,6 +20,7 @@ const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 const queryClient = new QueryClient()
+const showNewOrgButton = false
 
 root.render(
   <React.StrictMode>
@@ -37,9 +39,9 @@ root.render(
                 <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
                 </a>
               </div>
-              {/* <div>
+              {showNewOrgButton && <div>
                 <Link to="newOrg"><div className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Submit New Organization</div></Link>
-              </div> */}
+              </div>}
             </div>
           </nav>
           <Routes>

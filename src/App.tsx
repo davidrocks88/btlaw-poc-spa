257e3bed7c496d91c1f9335cefa4c8ff12300527
davidrocks88 <D1a0v9i4dX1e0n9o4>
2022-09-28 08:58:36 -0500

    update copy

--- src/App.tsx
+++ src/App.tsx
@@ -44,19 +44,18 @@ export function App() {
   return (
     <div>
       <div className='p-2 bg-gray-200 m-1 border'>
-        <p>Welcome to the pro bono program and opportunities Finder. This website will help you find pro bono programs and opportunities in the areas in which you are interested.</p>
+        <p>Welcome to the BT pro bono program and opportunities Finder. This website will help you find pro bono programs and opportunities in the areas in which you are interested.</p>
         <br />
         <p>How to use the Finder:</p>
-        <p className='pl-2'>1. Click on one or more "tags" with areas in which you are interested. Or, use the search bar below the tags. Click Reset to clear the selected tags.</p>
-        <p className='pl-2'>2. For all the tags you've selected, cards will appear below. Each card is a pro bono program which offers opportunities in selected areas.</p>
-        <p className='pl-2'>3. For each card, the name of the organization is a link to its website. Click on the Volunteer Now button to send an email directly to the volunteer coordinator at that organization. Cards which have Various as a tag have a large number of areas of law/interest, and not all may be listed.</p>
-        <p className='pl-2'>4. Click More to learn about training, geographic areas served, contact information, B&T contact person, etc. Click on Find Current Opportunities to link to the organization's website with more information.</p>
-        <p className='pl-2'>5. You can browse all cards by not selecting any tags.</p>
-
+        <p className='pl-2'>1. Click on one or more "tags" in the column on the left with areas in which you are interested. Or, use the search bar above the tags. Click Reset to clear the selected tags.</p>
+        <p className='pl-2'>2. For all the tags you've selected, cards will appear on the right. Each card is a pro bono program which offers opportunities in selected areas. You can browse all cards by not selecting any tags.</p>
+        <p className='pl-2'>3. For each card, the name of the organization is a link to its website. Cards which have Various as a tag have a large number of areas of law/interest, and not all may be listed. Click on the Volunteer Now button to send an email directly to the volunteer coordinator at that organization. </p>
+        <p className='pl-2'>4. Click More to learn about training, geographic areas served, contact information, B&T contact person (if any), etc. Click on Find Current Opportunities to link to the organization's website with more information.</p>
         <br />
         <p>
           Cards have as much information as we currently have; some may not have all areas of interest tagged or have full information.
         </p>
+        <p className='text-xs mt-1'>(c) Copyright 2022 Barnes & Thornburg LLP All Rights Reserved</p>
 
       </div>
       <div className="relative w-1/2 m-2 grow">
@@ -67,13 +66,13 @@ export function App() {
           <input onChange={(event) => setSearchString(event.target.value)} type="search" id="default-search" className="block p-4 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search Organization, Descriptions, Tags..." required />
         </div>
       </div>
-      <div className='grid grid-flow-col grid-cols-8 justify-start content-start items-start bg-gray-100'>
+      <div className='grid grid-flow-col grid-cols-7 justify-start content-start items-start bg-gray-100'>
         <div className='pt-2 flex flex-row flex-wrap gap-y-2 items-end align-middle content-center'>
           <div className='cursor-pointer hover:bg-gray-200 rounded-full p-2 font-lg font-bold mb-[2px]' onClick={() => setFilters({})}>Reset</div>
           {tags.map(t => <div key={t} className='mb-3'><Tag filters={filters} name={t} handleClick={updateFilter} /></div>)}
         </div>
 
-        <div className='col-span-7 flex flex-row flex-wrap'>
+        <div className='col-span-6 flex flex-row flex-wrap'>
           {orgsToShow?.sort((a, b) => (a.name < b.name ? -1 : 1)).map(org => <Organization organization={org} />)}
         </div>
       </div>

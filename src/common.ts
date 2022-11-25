7ce8744089590901c922bedc5a12d5b9718f394e
davidrocks88 <D1a0v9i4dX1e0n9o4>
2022-11-24 23:12:35 -0500

    More lint

--- src/common.ts
+++ src/common.ts
@@ -1,10 +1,7 @@
 export function toTitleCase(str: string) {
-  return str.replace(
-    /\w\S*/g,
-    function(txt) {
-      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
-    }
-  );
+  return str.replace(/\w\S*/g, function (txt) {
+    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+  })
 }
 
 export const BASE_URL = 'https://kwihtaig61.execute-api.us-east-1.amazonaws.com'
--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import { App } from './App';
-import reportWebVitals from './reportWebVitals';
-import {
-  BrowserRouter,
-  Link,
-  Routes,
-  Route
-} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { App } from './App'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter, Link, Routes, Route } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { OrgEditor } from './OrgEditor'
+import awsExports from './aws-exports'
+import { Amplify } from 'aws-amplify'
+import { Migration } from './Migration'
+import { Test } from './Test'
 
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-import { OrgEditor } from './OrgEditor';
-import awsExports from './aws-exports';
-import { Amplify } from 'aws-amplify';
-import { Migration } from './Migration';
-import { Test } from './Test';
+Amplify.configure(awsExports)
 
-Amplify.configure(awsExports);
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 const queryClient = new QueryClient()
 const showNewOrgButton = false
 
@@ -33,41 +22,62 @@ root.render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <div className=''>
-          <nav className="flex items-center justify-between flex-wrap bg-[#131829] p-6">
-            <div className="flex items-center flex-shrink-0 text-white mr-6">
-              <a href='https://btlaw.com' target='_blank' rel="noreferrer">
-                <img alt="btlaw icon" className='h-8' src='https://btlaw.com/-/media/images/btlaw/site-logo/btlawlogo.ashx?h=69&w=411&la=en&hash=C3C4E5F6805DC4719AF72237AD0B82E1' />
+          <nav className='flex items-center justify-between flex-wrap bg-[#131829] p-6'>
+            <div className='flex items-center flex-shrink-0 text-white mr-6'>
+              <a href='https://btlaw.com' target='_blank' rel='noreferrer'>
+                <img
+                  alt='btlaw icon'
+                  className='h-8'
+                  src='https://btlaw.com/-/media/images/btlaw/site-logo/btlawlogo.ashx?h=69&w=411&la=en&hash=C3C4E5F6805DC4719AF72237AD0B82E1'
+                />
               </a>
-              <span className="ml-4 font-semibold text-xl tracking-tight">Pro Bono Finder™</span>
+              <span className='ml-4 font-semibold text-xl tracking-tight'>Pro Bono Finder™</span>
             </div>
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div className="text-sm lg:flex-grow">
-                <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4">
+            <div className='w-full block flex-grow lg:flex lg:items-center lg:w-auto'>
+              <div className='text-sm lg:flex-grow'>
+                <a
+                  href='#responsive-header'
+                  className='block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4'
+                >
+                  {''}
                 </a>
               </div>
-              {showNewOrgButton && <div>
-                <Link to="newOrg"><div className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Submit New Organization</div></Link>
-              </div>}
-              <a href="mailto:jason.bernstein@btlaw.com?Subject=Pro Bono Finder Feedback" target='_blank' rel="noreferrer"><div className="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0">Give Us Feedback</div></a>
-
+              {showNewOrgButton && (
+                <div>
+                  <Link to='newOrg'>
+                    <div className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'>
+                      Submit New Organization
+                    </div>
+                  </Link>
+                </div>
+              )}
+              <a
+                href='mailto:jason.bernstein@btlaw.com?Subject=Pro Bono Finder Feedback'
+                target='_blank'
+                rel='noreferrer'
+              >
+                <div className='inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0'>
+                  Give Us Feedback
+                </div>
+              </a>
             </div>
           </nav>
           <Routes>
-            <Route path="/">
+            <Route path='/'>
               <Route index element={<App />} />
-              <Route path="newOrg" element={<OrgEditor />} />
-              <Route path="newOrg/:id" element={<OrgEditor />} />
-              <Route path="migration" element={<Migration />} />
-              <Route path="test" element={<Test />} />
+              <Route path='newOrg' element={<OrgEditor />} />
+              <Route path='newOrg/:id' element={<OrgEditor />} />
+              <Route path='migration' element={<Migration />} />
+              <Route path='test' element={<Test />} />
             </Route>
           </Routes>
         </div>
       </BrowserRouter>
     </QueryClientProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
